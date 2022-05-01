5f12e94677a843710a824e55ee422d612c8b6f51
Baymax <mulanohs@gmail.com>
2022-05-01 12:55:30 +0900

    Create config.php

---
+++ main/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$sname= "localhost";
+
+$unmae= "root";
+
+$password = "";
+
+$db_name = "smssend";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+
+}
