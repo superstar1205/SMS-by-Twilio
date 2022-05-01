42754df497d185c60e593ad3443aa05d1aa481cc
Baymax <mulanohs@gmail.com>
2022-05-01 12:35:21 +0900

    aaa

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '327ef9637bd3952205255a4d9789aa17028d2b03',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '327ef9637bd3952205255a4d9789aa17028d2b03',
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.36.1',
+            'version' => '6.36.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'reference' => '68aa5dd58450d3b449c83138567ecfca93955ed9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
